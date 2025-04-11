8477738e8ee6abae222c2ce44760bb19f24b5b5f
BennettJamesBishop <bennettjamesbishop@gmail.com>
2025-04-10 23:38:19 -0700

    Fix one inconsistency

--- scripts/vectorstore/figures/MMD/MMDFigureDescriptions.py
+++ scripts/vectorstore/figures/MMD/MMDFigureDescriptions.py
@@ -21,7 +21,7 @@ def generate_descriptions_of_MMD_figures():
     client = OpenAI(api_key=openai_api_key)
 
     # Generate descriptions for each figure using the function below, add them to dataframe
-    context_df['Description'] = context_df.apply(lambda row: generate_figure_description(client, row['Figure Number'], row['Context']), axis=1)
+    context_df['Image Description'] = context_df.apply(lambda row: generate_figure_description(client, row['Figure Number'], row['Context']), axis=1)
     
     # Remove the 'Context' column from the DataFrame
     descriptions_df = context_df.drop(columns=['Context'])
