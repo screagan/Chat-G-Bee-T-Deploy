5091db2a37cbb061673a28b8dcac548734e1d562
BennettJamesBishop <bennettjamesbishop@gmail.com>
2025-04-28 22:56:06 -0700

    Updated data with page nums

--- scripts/vectorstore/createEmbeddingDataframes.py
+++ scripts/vectorstore/createEmbeddingDataframes.py
@@ -17,7 +17,7 @@ if __name__ == "__main__":
 # First: Get text chunks and page numbers from main text of MMD, put them into DF ready to be embedded
     mmd_extracted_texts = extract_text_from_pdf(object_key='MMD-Main-Text.pdf', bucket_name='ccber-tester-bucket')
     mmd_text_chunks_df = chunk_text_to_dataframe(extracted_texts=mmd_extracted_texts, source_name="MMD")
-
+    mmd_text_chunks_df.to_csv("data/dataframesForEmbeddings/mmd_text_chunks.csv", index=False)
 # Second: Get text chunks and page numbers from Hymenoptera, put them into DF ready to be embedded
 # hymenoptera_extracted_texts = extract_text_from_pdf(object_key='Hymenoptera-Text', bucket_name='ccber-tester-bucket')
 # hymenoptera_text_chunks_df = chunk_text_to_dataframe(extracted_texts=mmd_extracted_texts, source_name="Hymenoptera")
@@ -31,18 +31,18 @@ if __name__ == "__main__":
 
 # Third: Generate descriptions of MMD Figures, put them into DF ready to be embedded
     mmd_figs_df = generate_descriptions_of_MMD_figures()
-    mmd_figs_df.to_csv("data/mmd_figs.csv", index=False)
+    mmd_figs_df.to_csv("data/dataframesForEmbeddings/mmd_figs.csv", index=False)
     print(list(mmd_figs_df.columns))
 
 # Fourth: Generate descriptions of Hymenoptera Figures, put them into DF ready to be embedded
     hymenoptera_figs_df = generate_descriptions_of_hymenoptera_figures()
-    hymenoptera_figs_df.to_csv("data/hymenoptera_figs.csv", index=False)
+    hymenoptera_figs_df.to_csv("data/dataframesForEmbeddings/hymenoptera_figs.csv", index=False)
     print(list(hymenoptera_figs_df.columns))
 
 # Combine all DataFrames into a single DataFrame
     combined_df = pd.concat([mmd_text_chunks_df, mmd_figs_df, hymenoptera_figs_df], ignore_index=True)
 
 # Save the combined DataFrame to a single CSV file
-    combined_df.to_csv("data/combined_data.csv", index=False)
+    combined_df.to_csv("data/dataframesForEmbeddings/combined_data.csv", index=False)
     print("Combined DataFrame saved to 'data/combined_data.csv'")
 
