1ef5462ac05e4f6c62f2c603f7d6588eb930e3f4
BennettJamesBishop <bennettjamesbishop@gmail.com>
2025-05-16 22:49:35 -0700

    Last commit of the day, renaming variables for clarity

--- scripts/vectorstore/createEmbeddingDataframes.py
+++ scripts/vectorstore/createEmbeddingDataframes.py
@@ -9,8 +9,8 @@ if __name__ == "__main__":
 # Get text chunks and page numbers from main text of MMD, put them into DF ready to be embedded
 # We do not use text chunks from the keys text of MMD, because it is not useful for embeddings
 # The information within the keys text of MMD is accessible to the chatbots through figure descriptions
-    mmd_extracted_texts = extract_text_from_pdf(object_key='MMD-Main-Text.pdf', bucket_name='ccber-tester-bucket', num_columns=1)
-    mmd_text_chunks_df = chunk_main_text_and_put_into_dataframe(extracted_texts=mmd_extracted_texts, source_title='MMD', source_year='2022', source_author='Danforth, Bryan, et al.', source_publisher='MMDPublisher')
+    mmd_extracted_main_text = extract_text_from_pdf(object_key='MMD-Main-Text.pdf', bucket_name='ccber-tester-bucket', num_columns=1)
+    mmd_text_chunks_df = chunk_main_text_and_put_into_dataframe(extracted_texts=mmd_extracted_main_text, source_title='MMD', source_year='2022', source_author='Danforth, Bryan, et al.', source_publisher='MMDPublisher')
     mmd_text_chunks_df.to_csv("data/dataframesForEmbeddings/mmd_text_chunks.csv", index=False) #Save to csv to check out chunks. Optional.
     print("Made text chunks for MMD")
 
@@ -21,8 +21,8 @@ if __name__ == "__main__":
 
 # Get text chunks and page numbers from HOTW, put them into DF ready to be embedded
 # Like MMD, we do not use text chunks from the keys text of HOTW, all of that info is in the figure descriptions
-    hymenoptera_extracted_texts = extract_text_from_pdf(object_key='HOTW-Main-Text.pdf', bucket_name='ccber-tester-bucket', num_columns=2) #TODO: This pdf is kinda off center. Check text extraction.
-    hymenoptera_text_chunks_df = chunk_main_text_and_put_into_dataframe(extracted_texts=hymenoptera_extracted_texts, source_title="Hymenoptera of the World", source_year='1993', source_author='Charles D. Michener', source_publisher='HOTWPublisher')
+    hymenoptera_extracted_main_text = extract_text_from_pdf(object_key='HOTW-Main-Text.pdf', bucket_name='ccber-tester-bucket', num_columns=2) #TODO: This pdf is kinda off center. Check text extraction.
+    hymenoptera_text_chunks_df = chunk_main_text_and_put_into_dataframe(extracted_texts=hymenoptera_extracted_main_text, source_title="Hymenoptera of the World", source_year='1993', source_author='Charles D. Michener', source_publisher='HOTWPublisher')
     hymenoptera_text_chunks_df.to_csv("data/dataframesForEmbeddings/hymenoptera_text_chunks.csv", index=False)
     print("Made text chunks for HOTW")
 
@@ -32,11 +32,13 @@ if __name__ == "__main__":
     print(list(hymenoptera_figs_df.columns))
 
 # Get text chunks and page numbers from BOTW, put them into DF ready to be embedded 
-# TODO: Get only main text, right now you just have small sample object instead of proper all main text.
-    botw_extracted_texts = extract_text_from_pdf(object_key='test_short_botw.pdf', bucket_name='ccber-tester-bucket', num_columns=2)
-    botw_text_chunks_df = chunk_main_text_and_put_into_dataframe(extracted_texts=botw_extracted_texts, source_title="Bees of the World", source_year='2007', source_author='Daniel C. Danforth', source_publisher='BOTWPublisher')
+# TODO: Get full main text only, right now you just have small sample pages instead of proper entirety of main text.
+    botw_extracted_main_text = extract_text_from_pdf(object_key='short_BOTW_Main_Text.pdf', bucket_name='ccber-tester-bucket', num_columns=2)
+    botw_text_chunks_df = chunk_main_text_and_put_into_dataframe(extracted_texts=botw_extracted_main_text, source_title="Bees of the World", source_year='2007', source_author='Daniel C. Danforth', source_publisher='BOTWPublisher')
     botw_text_chunks_df.to_csv("data/dataframesForEmbeddings/short_test_botw_text_chunks.csv", index=False)
     print("Made text chunks for (shorter version of) BOTW")
+
+# TODO: Add an BOTW keys chunk section, because unlike MMD and HOTW, BOTW has a keys section that is useful for embeddings.
 
 # Generate descriptions of BOTW Figures, put them into DF ready to be embedded 
     # botw_figs_df = TODO: Make this function actually work to make figure descriptions for BOTW
